353d1156a6cd30833ffe286b410d87fa0cb27b57
Jeremykalar <jeremykalar@gmail.com>
2021-12-22 17:58:10 -0500

    Update charas2.java

--- java/overwatch-rng/charas2.java
+++ java/overwatch-rng/charas2.java
@@ -4,50 +4,12 @@ import java.util.Scanner;
 public class charas2 {
 	static Random rand = new Random();
 	static Scanner scan = new Scanner(System.in);
-	static String[] t = new String[8];
-	static String[] d = new String[17];
-	static String[] s = new String[7];
+	static String[] t =  {"d.va", "orisa","reinheart", "Roadhog","Sigma","windston", "Wreking ball","zarya"};
+	static String[] d = { "Ashe", "Bastion", "Cassidy", "Echo", "Doomfist", "Genji", "Hanzo", "Junkrat", "Mei",
+			"Pharah", "Reaper", "Soldier: 76", "Sombra", "Symmetra", "Torbjörn", "Tracer", "Widowmaker" };
+	static String[] s = { "Ana","Baptiste","Brigitte","Lúcio", "Mercy", "Moira", "Zenyatta",};
 
-	public static void arry() {
-		// tanks
-		t[0] = "d.va";
-		t[1] = "orisa";
-		t[2] = "reinheart";
-		t[3] = "Roadhog";
-		t[4] = "Sigma";
-		t[5] = "windston";
-		t[6] = "Wreking ball";
-		t[7] = "zarya";
 
-		// dps
-		d[0] = "Ashe";
-		d[1] = "Bastion";
-		d[2] = "Cassidy";
-		d[3] = "Echo";
-		d[4] = "Doomfist";
-		d[5] = "Genji";
-		d[6] = "Hanzo";
-		d[7] = "Junkrat";
-		d[8] = "Mei";
-		d[9] = "Pharah";
-		d[10] = "Reaper";
-		d[11] = "Soldier: 76";
-		d[12] = "Sombra";
-		d[13] = "Symmetra";
-		d[14] = "Torbjörn";
-		d[15] = "Tracer";
-		d[16] = "Widowmaker";
-
-		// support
-		s[0] = "Ana";
-		s[1] = "Baptiste";
-		s[2] = "Brigitte";
-		s[3] = "Lúcio";
-		s[4] = "Mercy";
-		s[5] = "Moira";
-		s[6] = "Zenyatta";
-
-	}
 
 	public static void overwatch(String role) {
 		// should be print out the arrays above
@@ -75,11 +37,7 @@ public class charas2 {
 
 	public static void main(String[] args) {
 		String role = "dps";
-	//	role = print();
-		arry();
-if (role != "tanks" || role != "dps" || role != "suppot") 
-{ System.out.println("ERROR");
-		overwatch(role);
+		// role = print();
+			overwatch(role);
+		}
 	}
-}
-}
