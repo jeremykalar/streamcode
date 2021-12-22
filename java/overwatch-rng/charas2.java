3ecba2b1810318fd4270cb210ad3960269255489
Jeremykalar <jeremykalar@gmail.com>
2021-12-21 21:02:15 -0500

    almost working
    changed tiers to arrays and print the arrays

--- java/overwatch-rng/charas2.java
+++ java/overwatch-rng/charas2.java
@@ -1,90 +1,78 @@
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class charas2 {
 	static Random rand = new Random();
 	static Scanner scan = new Scanner(System.in);
-	static String[] t = new String[9];
-	static String[] d = new String[18];
-	static String[] s = new String[8];
+	static String[] t = new String[8];
+	static String[] d = new String[17];
+	static String[] s = new String[7];
 	
 	public static void arry() {
 		// tanks
-		t[1] = "d.va";
-		t[2] = "orisa";
-		t[3] = "reinheart";
-		t[4] = "Roadhog";
-		t[5] = "Sigma";
-		t[6] = "windston";
-		t[7] = "Wreking ball";
-		t[8] = "zarya";
+		t[0] = "d.va";
+		t[1] = "orisa";
+		t[2] = "reinheart";
+		t[3] = "Roadhog";
+		t[4] = "Sigma";
+		t[5] = "windston";
+		t[6] = "Wreking ball";
+		t[7] = "zarya";
 
 		// dps
-		d[1] = "Ashe";
-		d[2] = "Bastion";
-		d[3] = "Cassidy";
-		d[4] = "Echo";
-		d[5] = "Doomfist";
-		d[6] = "Genji";
-		d[7] = "Hanzo";
-		d[8] = "Junkrat";
-		d[9] = "Mei";
-		d[10] = "Pharah";
-		d[11] = "Reaper";
-		d[12] = "Soldier: 76";
-		d[13] = "Sombra";
-		d[14] = "Symmetra";
-		d[15] = "Torbjörn";
-		d[16] = "Tracer";
-		d[17] = "Widowmaker";
+		d[0] = "Ashe";
+		d[1] = "Bastion";
+		d[2] = "Cassidy";
+		d[3] = "Echo";
+		d[4] = "Doomfist";
+		d[5] = "Genji";
+		d[6] = "Hanzo";
+		d[7] = "Junkrat";
+		d[8] = "Mei";
+		d[9] = "Pharah";
+		d[10] = "Reaper";
+		d[11] = "Soldier: 76";
+		d[12] = "Sombra";
+		d[13] = "Symmetra";
+		d[14] = "Torbjörn";
+		d[15] = "Tracer";
+		d[16] = "Widowmaker";
 
 		// support
-		s[1] = "Ana";
-		s[2] = "Baptiste";
-		s[3] = "Brigitte";
-		s[4] = "Lúcio";
-		s[5] = "Mercy";
-		s[6] = "Moira";
-		s[7] = "Zenyatta";
+		s[0] = "Ana";
+		s[1] = "Baptiste";
+		s[2] = "Brigitte";
+		s[3] = "Lúcio";
+		s[4] = "Mercy";
+		s[5] = "Moira";
+		s[6] = "Zenyatta";
+
+	
+		
 	}
 
-	public static String overwatch(String role) {
+	public static void overwatch(String role) {
 		
-		int number = rand.nextInt(8);
-		String output = "test";
-		output = output + number;
+		//int number = rand.nextInt(8);
 		if (role == "tanks") {
-			output = (String)t[number];
+			for(int i = 0; i < t.length; i++) {
+				System.out.println(t[i]);
+			}
 		}
 		if (role == "dps") {
-			output = (String) d[number];
+			for(int i = 0; i < d.length; i++) {
+				System.out.println(d[i]);
+			}
 		}
 		if (role == "support") {
-			output = (String) s[number];
-		}
+			for(int i = 0; i < s.length; i++) {
+				System.out.println(s[i]);
+				}
+			}
+
 		//System.out.println(input);
-		System.out.println(number);
-		System.out.println(output);
-
-		/*
-		 * //tanks String t1 = "d.va"; String t2 = "orisa"; String t3 = "reinheart";
-		 * String t4 = "Roadhog"; String t5 = "Sigma"; String t6 = "windston"; String t7
-		 * = "Wreking ball"; String t8 = "zarya";
-		 * 
-		 * //dps String d1 = "Ashe"; String d2 = "Bastion"; String d3 = "Cassidy";
-		 * String d4 = "Echo"; String d5 = "Doomfist"; String d6 = "Genji"; String d7 =
-		 * "Hanzo"; String d8 = "Junkrat"; String d9 = "Mei"; String d10 = "Pharah";
-		 * String d11 = "Reaper"; String d12 = "Soldier: 76"; String d13 = "Sombra";
-		 * String d14 = "Symmetra"; String d15 = "Torbjörn"; String d16 = "Tracer";
-		 * String d17 = "Widowmaker";
-		 * 
-		 * //support String s1 = "Ana"; String s2 = "Baptiste"; String s3 = "Brigitte";
-		 * String s4 = "Lúcio"; String s5 = "Mercy"; String s6 = "Moira"; String s7 =
-		 * "Zenyatta";
-		 */
-
-		return output;
+		
+		//return output;
 	}
 
 	public static void main(String[] args) {
@@ -93,9 +81,8 @@ public class charas2 {
 		System.out.println("Here is what you entered: " + role);
 		System.out.println();
 		arry();
-		String end;
-		end = overwatch(role);
-		System.out.println(end);
+		overwatch(role);
+	
 
 	}
 
